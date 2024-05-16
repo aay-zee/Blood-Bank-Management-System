ddfdedb3b2220987683806de64a2b09a126018ec
ezza-abdullah1 <ezza.abdullahh@gmail.com>
2024-05-17 00:47:56 +0500

    final changes

--- src/blood/bank/system/PreDonationHistory.java
+++ src/blood/bank/system/PreDonationHistory.java
@@ -7,61 +7,60 @@ import java.awt.event.ActionListener;
 
 public class PreDonationHistory extends JFrame implements ActionListener {
 
-    private JButton donors,recipients,matches;
+    private JButton donors, recipients, matches, inventory;
 
     PreDonationHistory() {
         super("Pre Donation History");
 
-        JLabel label1=new JLabel("Blood Bank Management System");
-        label1.setBounds(420,20,400,40);
-        label1.setFont(new Font("Raleway",Font.BOLD,25));
+        JLabel label1 = new JLabel("Blood Bank Management System");
+        label1.setBounds(420, 20, 400, 40);
+        label1.setFont(new Font("Raleway", Font.BOLD, 25));
         add(label1);
 
-
-
-        donors=new JButton("View Donors");
-        donors.setFont(new Font("Raleway",Font.BOLD,14));
+        donors = new JButton("View Donors");
+        donors.setFont(new Font("Raleway", Font.BOLD, 14));
         donors.setBackground(Color.BLACK);
         donors.setForeground(Color.WHITE);
-        donors.setBounds(515,150,200,30);
+        donors.setBounds(515, 120, 200, 30);
         donors.addActionListener(this);
         add(donors);
 
-        recipients=new JButton("View Recipients");
-        recipients.setFont(new Font("Raleway",Font.BOLD,14));
+        recipients = new JButton("View Recipients");
+        recipients.setFont(new Font("Raleway", Font.BOLD, 14));
         recipients.setBackground(Color.BLACK);
         recipients.setForeground(Color.WHITE);
-        recipients.setBounds(515,200,200,30);
+        recipients.setBounds(515, 170, 200, 30);
         recipients.addActionListener(this);
         add(recipients);
 
-        matches=new JButton("View Cross Matches");
-        matches.setFont(new Font("Raleway",Font.BOLD,14));
+        matches = new JButton("View Donation History");
+        matches.setFont(new Font("Raleway", Font.BOLD, 14));
         matches.setBackground(Color.BLACK);
         matches.setForeground(Color.WHITE);
-        matches.setBounds(515,250,200,30);
+        matches.setBounds(515, 220, 200, 30);
         matches.addActionListener(this);
         add(matches);
 
+        inventory = new JButton("View Blood Inventory");
+        inventory.setFont(new Font("Raleway", Font.BOLD, 14));
+        inventory.setBackground(Color.BLACK);
+        inventory.setForeground(Color.WHITE);
+        inventory.setBounds(515, 270, 200, 30);
+        inventory.addActionListener(this);
+        add(inventory);
 
-
-
-
-
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/LOGO.png"));
-        Image i2=i1.getImage().getScaledInstance(850,480,Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
-        JLabel image=new JLabel(i3);
-        image.setBounds(0,0,850,480);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icon/LOGO.png"));
+        Image i2 = i1.getImage().getScaledInstance(850, 480, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 850, 480);
         add(image);
 
-
         setLayout(null);
-        //getContentPane().setBackground(new Color(246, 205, 205));
+        // getContentPane().setBackground(new Color(246, 205, 205));
         getContentPane().setBackground(new Color(255, 255, 255));
-        setSize(850,480);
-        setLocation(360,200);
-
+        setSize(850, 480);
+        setLocation(360, 200);
 
         setVisible(true);
 
@@ -69,20 +68,21 @@ public class PreDonationHistory extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try{
-            if(e.getSource()==donors){
+        try {
+            if (e.getSource() == donors) {
                 setVisible(false);
                 new DonationHistory("donors");
-            }
-            else if(e.getSource()==recipients){
+            } else if (e.getSource() == recipients) {
                 setVisible(false);
                 new DonationHistory("recipients");
-            }
-            else{
+            } else if (e.getSource() == matches) {
                 setVisible(false);
                 new DonationHistory("matches");
+            } else {
+                setVisible(false);
+                new DonationHistory("inventory");
             }
-        }catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
